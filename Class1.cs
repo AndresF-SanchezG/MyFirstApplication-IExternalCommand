3b2b2dbe5936a6ed777dbde3ef9a75b64ef73a73
Andrés Sánchez <113924667+AndresF-SanchezG@users.noreply.github.com>
2024-07-08 19:28:27 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace RevitAPP
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Titulo","Mi primera APP con Revit y C#");
+            return Result.Succeeded;
+        }
+    }
+}
